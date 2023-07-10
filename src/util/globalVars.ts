84f9b75dd18177f5037080f7b11901b1ef6190e1
Jann Louie Almirante <jannlouiealmirante@gmail.com>
2023-07-11 00:19:17 +0800

    Change backend url

--- src/util/globalVars.ts
+++ src/util/globalVars.ts
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
-export const backendURL = "http://127.0.0.1:8000";
-// export const backendURL = "https://octopus-app-fjfrx.ondigitalocean.app";
+// export const backendURL = "http://127.0.0.1:8000";
+export const backendURL = "https://octopus-app-fjfrx.ondigitalocean.app";
 
 const configuration = new Configuration({
   apiKey: "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",

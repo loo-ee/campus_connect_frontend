72f55d501717d58f9e41501a6a36da41941d67f0
Jann Louie Almirante <jannlouiealmirante@gmail.com>
2023-07-10 23:37:53 +0800

    Linting checks

--- src/pages/login/LoginTab.tsx
+++ src/pages/login/LoginTab.tsx
@@ -104,7 +104,7 @@ function LoginTab() {
           </fieldset>
           <div className="flex justify-evenly mt-5">
             <button
-              onClick={(e) => navigate("/")}
+              onClick={() => navigate("/")}
               className="inline-flex items-center justify-center rounded px-[15px] text-[15px] leading-none font-medium h-[35px] bg-red-400 text-red-700 hover:bg-red-500 focus:shadow-[0_0_0_2px] focus:shadow-green7 outline-none cursor-default"
             >
               Login as Guest
